d5c67b0432085499824fb9f93f7d8673de8ad512
Aeropagz <klaas.pelzer@gmx.de>
2020-09-24 14:08:29 +0200

    graph added

--- server/backend-api/test-client.py
+++ server/backend-api/test-client.py
@@ -1,9 +1,15 @@
 import requests
-import os
+import random
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://127.0.0.1:5000/co2/Raum2"
 
-#response = requests.get(BASE + "helloworld")
-#print(response.json())
-path = os.path.dirname(__file__)
-print(path)
+def randomData():
+    hours = random.randint(0,23)
+    min = random.randint(0,59)
+    time = str(hours)+":"+str(min)
+    value = random.randint(0, 1800)
+    return {"time": time, "value": value}
+
+
+response = requests.post(BASE, data=randomData())
+print(response)
